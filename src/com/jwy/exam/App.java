1176327f2e63f74f47021503445c031c75e90eaf
wooyong99 <wulovesk@naver.com>
2022-12-18 14:08:14 +0900

    mysql connector 라이브러리 추가

--- src/com/jwy/exam/App.java
+++ src/com/jwy/exam/App.java
@@ -4,26 +4,25 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-  public void run(){
+  public void run() {
     int articleIdx = 0;
     Scanner sc = Container.scanner;
     String cmd;
     ArrayList<Article> articles = new ArrayList<Article>();
-    while(true){
+    while (true) {
       System.out.printf("명령어) ");
       cmd = sc.nextLine();
-      if (cmd.equals("article list")){
-        if(articles.isEmpty()){
+      if (cmd.equals("article list")) {
+        if (articles.isEmpty()) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
         System.out.println("== 게시물 리스트 ==");
         System.out.println("번호 / 제목");
-        for(Article article : articles){
+        for (Article article : articles) {
           System.out.printf("%d / %s\n", article.id, article.title);
         }
-      }
-      else if(cmd.equals("article write")){
+      } else if (cmd.equals("article write")) {
         System.out.println("== 게시물 작성 ==");
         int id = ++articleIdx;
         System.out.printf("제목 : ");
@@ -33,17 +32,14 @@ public class App {
         Article article = new Article(id, title, body);
         articles.add(article);
         System.out.printf("작성된 게시물 - %s\n", article.toString());
-        System.out.printf("%d번 게시물이 작성되었습니다.\n",id);
-      }
-      else if(cmd.equals("system exit")){
+        System.out.printf("%d번 게시물이 작성되었습니다.\n", id);
+      } else if (cmd.equals("system exit")) {
         System.out.println("== 시스템 종료 ==");
         break;
-      }
-      else{
+      } else {
         System.out.println("명령어를 확인해주세요.");
       }
     }
-
 
 
   }
