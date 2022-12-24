38709cd035d3b965748d226194b98b9110c04723
wooyong99 <wulovesk@naver.com>
2022-12-24 09:45:36 +0900

    mysql 커넥터 버전 변경 및 JDBC연결 테스트

---
+++ src/com/jwy/exam/test/JDBCTest.java
@@ -0,0 +1,33 @@
+package com.jwy.exam.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCTest {
+  public static void main(String[] args) {
+    Connection con = null;
+
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+
+      String url = "jdbc:mysql://localhost:3306/text_board";
+
+      con = DriverManager.getConnection(url, "jwy", "1234");
+      System.out.println("연결 성공");
+    } catch(ClassNotFoundException e) {
+      System.out.println("드라이버 로딩 실패");
+    }catch(SQLException e){
+      System.out.println("에러: "+ e);
+    }
+    finally{
+      try{
+        if( con != null && !con.isClosed()){
+          con.close();
+        }
+      }catch(SQLException e){
+        e.printStackTrace();
+      }
+    }
+  }
+}
