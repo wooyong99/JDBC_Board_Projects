72ac08610a239d119c1fdf13cf91c3ac8ff16b53
wooyong99 <wulovesk@naver.com>
2022-12-24 10:37:28 +0900

    JDBC Insert Test

--- db/db.sql
+++ db/db.sql
@@ -1,2 +1,19 @@
 DROP DATABASE IF EXISTS text_board;
 CREATE DATABASE text_board;
+USE text_board;
+
+CREATE TABLE article(
+	id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	title CHAR(100) NOT NULL,
+	`body` TEXT NOT NULL
+);
+
+SELECT * FROM article;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = "제목",
+`body` = "내용";
---
+++ src/com/jwy/exam/test/JDBCInsertTest.java
@@ -0,0 +1,43 @@
+package com.jwy.exam.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JDBCInsertTest {
+  public static void main(String[] args){
+    Connection con = null;
+    PreparedStatement psts = null;
+    try{
+      Class.forName("com.mysql.jdbc.Driver");
+      String url = "jdbc:mysql://localhost:3306/text_board";
+      con = DriverManager.getConnection(url, "jwy","1234");
+      System.out.println("연결 성공 !");
+
+      String sql = "INSERT INTO article";
+      sql += " SET regDate = NOW()";
+      sql += ", updateDate = NOW()";
+      sql += ", title = CONCAT(\"제목\",RAND())";
+      sql += ", `body` = CONCAT(\"내용\",RAND());";
+
+      psts = con.prepareStatement(sql);
+      int affectRows = psts.executeUpdate();
+
+      System.out.println("affectRows : " + affectRows);
+    }catch(ClassNotFoundException e){
+      System.out.println("연결 실패 !");
+    }catch(SQLException e){
+      System.out.println("에러 : " + e);
+    }
+    finally {
+      try{
+        if(con != null && !con.isClosed()){
+          con.close();
+        }
+      }catch(SQLException e){
+        e.printStackTrace();
+      }
+    }
+  }
+}
